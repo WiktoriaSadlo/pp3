09fd846d90d3c46fbaf9b599d357acb857eec6c6
WiktoriaSadlo <227960@student.uek.krakow.pl>
2023-10-24 14:48:01 +0200

    zadanka

---
+++ 03-ClassStructure/CinemaTicket.java
@@ -0,0 +1,15 @@
+public class CinemaTicket {
+
+    static String cinemaName = "Cinema City";
+    String movieTitle;
+    int row;
+    int seat;
+    float price;
+
+    static String toString() {
+        return cinemaName + ", movie title: '" + movieTitle +"' ";
+    }
+
+
+    
+}
